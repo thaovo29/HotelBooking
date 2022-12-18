ceaa656c63990b33930de264513d94f8ffac39ac
Thao_Vo <thaovo2962002@gmail.com>
2022-12-18 15:54:35 +0700

    Initial Commit

---
+++ HotelBooking/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotelBooking
+//
+//  Created by MacOS on 18/12/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
